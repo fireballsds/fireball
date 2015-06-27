4c4ca9999d58a08da269eb7b620ef2373191e326
shimdx <Shimdx@Shims-MacBook-Pro.local>
2015-06-27 20:44:45 +0530

    1st commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://fireball:1234qwer@ds035740.mongolab.com:35740/fireball'
+  },
+
+  seedDB: true
+};

839bf3063e6633e38f6327efbccecb3729c9fdf4
shimdx <Shimdx@Shims-MacBook-Pro.local>
2015-06-28 13:23:42 +0530

    commit
    tab

--- client/app/shimdx/shimdx.controller.js
+++ client/app/shimdx/shimdx.controller.js
@@ -3,4 +3,12 @@
 angular.module('fireballApp')
   .controller('ShimdxCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.tab = 1;
+    $scope.setTab = function(setTab){
+        $scope.tab = setTab;
+    };
+    $scope.isSet = function(checkTab){
+        console.log($scope.tab+' '+checkTab);
+        return $scope.tab === checkTab;
+    };
   });
